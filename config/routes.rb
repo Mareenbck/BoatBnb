fdb43f78b677e5516a680a48664a8c73a05c5996
Marine Bascunana <marine.bascunana@gmail.com>
2021-02-15 14:57:32 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :boats do
+    resources :reviews, only: [:new, :create, :destroy]
+    resources :reservations, except: :destroy
+  end
+
+  resources :dashboard, only: [:show]
+
 end
