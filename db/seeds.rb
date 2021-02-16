2ce0e3da35b4d18f7976ca682f49fb0bd9754d65
Marine Bascunana <marine.bascunana@gmail.com>
2021-02-16 14:36:35 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require 'faker'
-User.destroy_all
 Boat.destroy_all
+User.destroy_all
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
